9e6dbdeaa77b5f2800f4320fd2c5e71904451971
anushaka1467-code <anushaka1467@gmail.com>
2026-01-22 19:04:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openResume() {
+    document.getElementById("resume").style.display = "block";
+}
+
+function closeResume() {
+    document.getElementById("resume").style.display = "none";
+}
